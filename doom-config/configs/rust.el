e2e59547dc020244d046c75d5001b8521a420938
Kaushik Chakraborty <kaushik.chakraborty@macys.com>
2022-01-10 10:24:59 +0530

    mac update

---
+++ doom-config/configs/rust.el
@@ -0,0 +1,25 @@
+;;; rust.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Kaushik Chakraborty
+;;
+;; Author: Kaushik Chakraborty <https://github.com/kaychaks>
+;; Maintainer: Kaushik Chakraborty <kaushik.chakraborty3@cognizant.com>
+;; Created: January 03, 2022
+;; Modified: January 03, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/kaychaks/rust
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+;; (after! rustic
+;;   (setq rustic-lsp-server 'rls))
+
+;;; rust.el ends here
